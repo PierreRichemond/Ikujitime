706fbe283b0a2dbd4686860b99bc6357e4889cc9
= <pierreambroise@hotmail.fr>
2021-08-25 17:40:34 +0900

    carousel unfinished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 Rails.start()
 Turbolinks.start()
@@ -27,6 +29,23 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    },
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+  })
 });
 
 import "controllers"
