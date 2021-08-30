155a42e8bfcf0126d0d12e2364db374d7afbb64b
Daniel Rabottini <da_nkr@live.com>
2021-08-30 18:54:54 +0900

    stimulus map built

---
+++ app/javascript/controllers/build_map_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "stimulus"
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default class extends Controller {
+
+  static targets = ["tab","map"]
+
+  buildMap() {
+    setTimeout(() => {
+
+      if(this.mapbuilt !== true){
+        const mapElement = document.getElementById('map');
+        const fitMapToMarkers = (map, markers) => {
+          const bounds = new mapboxgl.LngLatBounds();
+          markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+          map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 });
+        };
+        if (mapElement) { // only build a map if there's a div#map to inject into
+          mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+          const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10'
+          });
+          const markers = JSON.parse(mapElement.dataset.markers);
+          markers.forEach((marker) => {
+            const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+            new mapboxgl.Marker()
+              .setLngLat([marker.lng, marker.lat])
+              .setPopup(popup)
+              .addTo(map);
+          });
+          fitMapToMarkers(map, markers);
+          this.mapbuilt = true
+        }
+      }
+    }, 500);
+  }
+}
