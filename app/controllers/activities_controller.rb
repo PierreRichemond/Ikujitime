f189a59bc9c01a20411e0a22f39e01ecaee08a00
= <pierreambroise@hotmail.fr>
2021-08-30 18:28:35 +0900

    selected only the upcoming events to add a gift or an activity to it

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
-    @events = Event.all.where(child_id: params[:child_id])
+    @events = Event.all.where(child_id: params[:child_id]).where( 'start_date > ?', Time.now )
     authorize @activity
   end
 end
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,7 +2,7 @@ class GiftsController < ApplicationController
 
   def show
     @gift = Gift.find(params[:id])
-    @events = Event.all.where(child_id: params[:child_id])
+    @events = Event.all.where(child_id: params[:child_id]).where( 'start_date > ?', Time.now )
     authorize @gift
   end
 end
