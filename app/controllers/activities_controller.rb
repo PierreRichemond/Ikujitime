9a0d14cc547f778448a1cb9c880b46e62b66837b
= <pierreambroise@hotmail.fr>
2021-08-31 15:25:28 +0900

    added style on activity and gift show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
 
   def show
+    @child = Child.find(params[:child_id])
     @activity = Activity.find(params[:id])
     @events = Event.all.where(child_id: params[:child_id]).where( 'start_date > ?', Time.now )
     authorize @activity
--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,6 +1,7 @@
 class GiftsController < ApplicationController
 
   def show
+    @child = Child.find(params[:child_id])
     @gift = Gift.find(params[:id])
     @events = Event.all.where(child_id: params[:child_id]).where( 'start_date > ?', Time.now )
     authorize @gift
